4c149a058efbbcd71a95e9aee7418b9acd2e3e2d
Farhad Afridi <afridifarhad149@gmail.com>
2024-08-04 01:27:52 +0500

    add more cards

--- src/conponents/FoodMenu.jsx
+++ src/conponents/FoodMenu.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import Card from '../assets/food1.jpg'
 function FoodMenu() {
   return (
+    <>
+    
+
+   
     <div className="food-menu">
         <h1>Food Menu</h1>
+        <div className="cards">
         <div className="all-cards"></div>
         <div className="menu-img">
          <img src={Card} alt="" />
@@ -15,13 +20,90 @@ function FoodMenu() {
             
         </div>
             <button>Order Now</button>
+            </div>
+
+
+
+
+  {/* card  2 */} 
+
+            <div className="all-cards"></div>
+        <div className="menu-img">
+         <img src={Card} alt="" />
+        
+        <div className="desc">
+            <h2>Food Title</h2>
+            <h3>$2.5</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            
         </div>
+            <button>Order Now</button>
+            </div>
 
 
 
+{/* card 3 */}
 
+<div className="all-cards"></div>
+        <div className="menu-img">
+         <img src={Card} alt="" />
+        
+        <div className="desc">
+            <h2>Food Title</h2>
+            <h3>$2.5</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            
+        </div>
+            <button>Order Now</button>
+            </div>
 
+            {/* card 4 */}
+
+            <div className="all-cards"></div>
+        <div className="menu-img">
+         <img src={Card} alt="" />
+        
+        <div className="desc">
+            <h2>Food Title</h2>
+            <h3>$2.5</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            
+        </div>
+            <button>Order Now</button>
+            </div>
+            {/* card 5 */}
+
+            <div className="all-cards"></div>
+        <div className="menu-img">
+         <img src={Card} alt="" />
+        
+        <div className="desc">
+            <h2>Food Title</h2>
+            <h3>$2.5</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            
+        </div>
+            <button>Order Now</button>
+            </div>
+            {/* card 6 */}
+            <div className="all-cards"></div>
+        <div className="menu-img">
+         <img src={Card} alt="" />
+        
+        <div className="desc">
+            <h2>Food Title</h2>
+            <h3>$2.5</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            
+        </div>
+            <button>Order Now</button>
+            </div>
+
+             </div>
+ 
     </div>
+    
+</>
   )
 }
 
