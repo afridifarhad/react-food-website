c752195079d604b149075deb09d52bbf9d07013c
Farhad Afridi <afridifarhad149@gmail.com>
2024-08-04 01:48:06 +0500

    fix some error

--- src/conponents/FoodMenu.jsx
+++ src/conponents/FoodMenu.jsx
@@ -1,110 +1,111 @@
 import React from 'react'
 import Card from '../assets/food1.jpg'
 function FoodMenu() {
-  return (
-    <>
-    
+    return (
+        <>
 
-   
-    <div className="food-menu">
-        <h1>Food Menu</h1>
-        <div className="cards">
-        <div className="all-cards"></div>
-        <div className="menu-img">
-         <img src={Card} alt="" />
-        
-        <div className="desc">
-            <h2>Food Title</h2>
-            <h3>$2.5</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            
-        </div>
-            <button>Order Now</button>
+
+
+            <div className="food-menu">
+                <h1>Food Menu</h1>
+                <div className="cards">
+                   
+                    <div className="menu-img">
+                        <img src={Card} alt="" />
+
+                        <div className="desc">
+                            <h2>Food Title</h2>
+                            <h3>$2.5</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                        <button>Order Now</button>
+                    </div>
+
+
+
+
+                    {/* card  2 */}
+
+                    
+                    <div className="menu-img">
+                        <img src={Card} alt="" />
+
+                        <div className="desc">
+                            <h2>Food Title</h2>
+                            <h3>$2.5</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                        <button>Order Now</button>
+                    </div>
+
+
+
+                    {/* card 3 */}
+
+                    
+                    <div className="menu-img">
+                        <img src={Card} alt="" />
+
+                        <div className="desc">
+                            <h2>Food Title</h2>
+                            <h3>$2.5</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                        <button>Order Now</button>
+                    </div>
+
+                    {/* card 4 */}
+
+                  
+                    <div className="menu-img">
+                        <img src={Card} alt="" />
+
+                        <div className="desc">
+                            <h2>Food Title</h2>
+                            <h3>$2.5</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                        <button>Order Now</button>
+                    </div>
+                    {/* card 5 */}
+
+                   
+                    <div className="menu-img">
+                        <img src={Card} alt="" />
+
+                        <div className="desc">
+                            <h2>Food Title</h2>
+                            <h3>$2.5</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                        <button>Order Now</button>
+                    </div>
+                    
+                    {/* card 6 */}
+                  
+                    <div className="menu-img">
+                        <img src={Card} alt="" />
+
+                        <div className="desc">
+                            <h2>Food Title</h2>
+                            <h3>$2.5</h3>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                        <button>Order Now</button>
+                    </div>
+
+                </div>
+                <h4>See All Foods</h4>
             </div>
 
-
-
-
-  {/* card  2 */} 
-
-            <div className="all-cards"></div>
-        <div className="menu-img">
-         <img src={Card} alt="" />
-        
-        <div className="desc">
-            <h2>Food Title</h2>
-            <h3>$2.5</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            
-        </div>
-            <button>Order Now</button>
-            </div>
-
-
-
-{/* card 3 */}
-
-<div className="all-cards"></div>
-        <div className="menu-img">
-         <img src={Card} alt="" />
-        
-        <div className="desc">
-            <h2>Food Title</h2>
-            <h3>$2.5</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            
-        </div>
-            <button>Order Now</button>
-            </div>
-
-            {/* card 4 */}
-
-            <div className="all-cards"></div>
-        <div className="menu-img">
-         <img src={Card} alt="" />
-        
-        <div className="desc">
-            <h2>Food Title</h2>
-            <h3>$2.5</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            
-        </div>
-            <button>Order Now</button>
-            </div>
-            {/* card 5 */}
-
-            <div className="all-cards"></div>
-        <div className="menu-img">
-         <img src={Card} alt="" />
-        
-        <div className="desc">
-            <h2>Food Title</h2>
-            <h3>$2.5</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            
-        </div>
-            <button>Order Now</button>
-            </div>
-            {/* card 6 */}
-            <div className="all-cards"></div>
-        <div className="menu-img">
-         <img src={Card} alt="" />
-        
-        <div className="desc">
-            <h2>Food Title</h2>
-            <h3>$2.5</h3>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            
-        </div>
-            <button>Order Now</button>
-            </div>
-
-             </div>
- 
-    </div>
-    
-</>
-  )
+        </>
+    )
 }
 
 export default FoodMenu
