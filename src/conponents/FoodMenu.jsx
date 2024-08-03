37be0f5f72f1560b3cbfbc1a30e1ad5b3ed00174
Farhad Afridi <afridifarhad149@gmail.com>
2024-08-04 01:10:54 +0500

    some style on card

--- src/conponents/FoodMenu.jsx
+++ src/conponents/FoodMenu.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
-
+import Card from '../assets/food1.jpg'
 function FoodMenu() {
   return (
     <div className="food-menu">
+        <h1>Food Menu</h1>
+        <div className="all-cards"></div>
         <div className="menu-img">
-
-        </div>
+         <img src={Card} alt="" />
+        
         <div className="desc">
             <h2>Food Title</h2>
             <h3>$2.5</h3>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            
         </div>
+            <button>Order Now</button>
+        </div>
+
+
+
+
+
     </div>
   )
 }
